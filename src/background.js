eb751c27a1e9767ebae56e007667a15f6d8ed74d
jason5ng32 <jason5ng32@gmail.com>
2023-10-31 21:33:59 +0800

    improve chatgpt delay time

--- src/background.js
+++ src/background.js
@@ -15,27 +15,23 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 function handleOpenUrlAndType(request) {
-  chrome.storage.sync.get(['modelType', 'delayTime'], function (data) {
+  chrome.storage.sync.get(['modelType'], function (data) {
     const modelType = data.modelType || 'gpt-4';
-    const delayTime = data.delayTime || 2000;
-
-    console.log("Received input:", decodeURIComponent(request.input));
 
     chrome.tabs.create({ url: `https://chat.openai.com/?model=${modelType}` }, (tab) => {
-      setTimeout(() => {
-        try {
-          chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            files: ['content.js']
-          }, () => {
-            chrome.tabs.sendMessage(tab.id, { action: 'typeInput', input: request.input });
-          });
-        } catch (error) {
-          console.error(error);
-        }
-      }, delayTime);
+      try {
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          files: ['content.js']
+        }, () => {
+          chrome.tabs.sendMessage(tab.id, { action: 'typeInput', input: request.input });
+        });
+      } catch (error) {
+        console.error(error);
+      }
     });
   });
 }
+
 
 
--- src/content.js
+++ src/content.js
@@ -1,13 +1,25 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   if (request.action === 'typeInput') {
     console.log('Received message to type input:', decodeURIComponent(request.input));
+
+    // 从 background.js 获取 delayTime
+    const { delayTime = 2000 } = await new Promise(resolve =>
+      chrome.storage.sync.get(['delayTime'], resolve)
+    );
+
+    // 添加延迟
+    await delay(delayTime);
+
     const textarea = document.querySelector('textarea[id="prompt-textarea"]');
     if (textarea) {
       textarea.value = decodeURIComponent(request.input);
       textarea.focus();  // 将焦点设置到文本区域
       textarea.setSelectionRange(textarea.value.length, textarea.value.length);  // 将光标移到文本的最末端
       textarea.dispatchEvent(new Event('input', { 'bubbles': true, 'cancelable': true }));
-
     }
   }
 });
